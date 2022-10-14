ae8c534b60fb31eb28f4793e68b670084fe0a3d1
Leduc20 <ducnhat31032002@gmail.com>
2022-10-15 01:00:52 +0700

    update code

---
+++ .history/admin/nav_20221014175408.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quản trị Website</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../content/icon/fontawesome-free-6.1.1-web/css/all.min.css">
+</head>
+
+<body>
+
+    <section class="flex px-2">
+        <div class="w-[20%]">
+            <ul>
+                <li class="ml-[10px] py-3 ml-1 border-[1px] bg-slate-300 rounded-[3px] font-bold hover:bg-white">
+                    <a class="no-underline text-black" href="index.php"><i class="fa-solid fa-store"></i>Trang chủ</a>
+                </li>
+                <li class="ml-[10px] py-3 ml-1 border-[1px] bg-slate-300 rounded-[3px] font-bold hover:bg-white">
+                    <a class="no-underline text-black" href="index.php?act=addlh"><i class="fa-solid fa-house"></i>Loại hàng</a>
+                </li>
+                <li class="ml-[10px] py-3 ml-1 border-[1px] bg-slate-300 rounded-[3px] font-bold hover:bg-white">
+                    <a class="no-underline text-black" href="index.php?act=addhh"><i class="fa-solid fa-cart-shopping"></i>Hàng hóa</a>
+                <li class="ml-[10px] py-3 ml-1 border-[1px] bg-slate-300 rounded-[3px] font-bold hover:bg-white">
+                    <a class="no-underline text-black" href="index.php?act=addkh"><i class="fa-brands fa-product-hunt"></i>Khách hàng</a>
+                </li>
+                <li class="ml-[10px] py-3 ml-1 border-[1px] bg-slate-300 rounded-[3px] font-bold hover:bg-white">
+                    <a class="no-underline text-black" href="index.php?act=dsbl"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+</svg>
+Bình luận</a>
+                </li>
+                <li class="ml-[10px] py-3 ml-1 border-[1px] bg-slate-300 rounded-[3px] font-bold hover:bg-white">
+                    <a class="no-underline text-black" href="index.php?act=dstk"><i class="fa-solid fa-users"></i>Thống kê</a>
+                </li>
+            </ul>
+        </div>
+        <!-- <div class="w-[80%]">
+            <div class="p-4">
+                <h2 class="text-[22px] font-bold text-center py-3">Danh sách sản phẩm</h2>
+            </div>
+        </div> -->
+    </section>
+</body>
+
+</html>
